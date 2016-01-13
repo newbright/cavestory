12a176aefaf25b0793f795c7a3f0cca856037466
Brandon Newbright <newbright.2@osu.edu>
2016-01-13 13:15:06 -0500

    Update to episode 4, 33 mins.

--- CaveStory/src/graphics.cpp
+++ CaveStory/src/graphics.cpp
@@ -1,11 +1,7 @@
 #include <SDL2/SDL.h>
-#include "graphics.h"
+#include <SDL2_image/SDL_image.h>
 
-/*
-*	Graphics class
-*  
-*	Holds all information dealing with graphics for the game.
-*/
+#include "graphics.h"
 
 Graphics::Graphics() {
 	SDL_CreateWindowAndRenderer(640, 480, 0, &this->_window, &this->_renderer);
@@ -14,4 +10,28 @@ Graphics::Graphics() {
 
 Graphics::~Graphics() {
 	SDL_DestroyWindow(this->_window);
+}
+
+SDL_Surface* Graphics::loadImage(const std::string &filePath) {
+	if (this->_spriteSheets.count(filePath) == 0) {
+		this->_spriteSheets[filePath] = IMG_Load(filePath.c_str());
+	}
+
+	return this->_spriteSheets[filePath];
+}
+
+void Graphics::blitSurface(SDL_Texture* texture, SDL_Rect* sourceRectangle, SDL_Rect* destinationRectangle) {
+	SDL_RenderCopy(this->getRenderer, texture, sourceRectangle, destinationRectangle);
+}
+
+void Graphics::flip() {
+	SDL_RenderPresent(this->getRenderer);
+}
+
+void Graphics::clear() {
+	SDL_RenderClear(this->getRenderer);
+}
+
+SDL_Renderer* Graphics::getRenderer() const {
+	return this->_renderer;
 }
